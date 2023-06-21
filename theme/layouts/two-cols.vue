8875f679c340a1ca998f1eafa8eecbbf424e5c9c
Eric Moore <EricGittaMoore@duck.com>
2023-06-21 11:30:02 +0800

    chore: init

---
+++ theme/layouts/two-cols.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="slidev-layout">
+    <div class="grid grid-cols-2 gap-16">
+      <div class="">
+        <slot name="default" />
+      </div>
+      <div class="">
+        <slot name="right"></slot>
+      </div>
+    </div>
+  </div>
+</template>
