286768140934e8724a9cbb76afb8d411307b8467
Eric Moore <EricGittaMoore@duck.com>
2023-06-21 11:50:59 +0800

    ci: fix baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import svgLoader from 'vite-svg-loader'
 
 export default defineConfig(({ command }) => {
   return {
+    base: command === 'build' ? '/ppt-cow-low-code/' : './',
     plugins: [svgLoader()],
   }
 })
